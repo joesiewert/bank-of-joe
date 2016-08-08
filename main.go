fcc0e4798098e2e333422ff6021aade1a3b2502d
Joe Siewert <joe.siewert@gmail.com>
2016-08-08 18:20:49 -0500

    Database setup

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-gorp/gorp"
+	_ "github.com/lib/pq"
 )
 
 func index(c *gin.Context) {
@@ -13,4 +19,20 @@ func main() {
 	router := gin.Default()
 	router.GET("/", index)
 	router.Run()
+}
+
+func initDb() *gorp.DbMap {
+	dburl := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", dburl)
+	checkErr(err, "sql.Open failed")
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
